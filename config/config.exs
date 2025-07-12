37645f135bcd05a0565c21a47600f2658f579316
pow <abinovalfauzi@proton.me>
2025-07-12 10:49:59 +0700

    🧹 chore: clean up phoenix boilerplate and migrate tailwind to v4

--- config/config.exs
+++ config/config.exs
@@ -43,15 +43,17 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.4.3",
+  version: "4.0.9",
   cafein: [
     args: ~w(
-      --config=tailwind.config.js
       --input=css/app.css
       --output=../priv/static/assets/app.css
     ),
     cd: Path.expand("../assets", __DIR__)
   ]
+
+# Configure heroicons
+config :ex_heroicons, type: "outline"
 
 # Configures Elixir's Logger
 config :logger, :console,
