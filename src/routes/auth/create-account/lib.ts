c4bb68983c8ff541725154fc9111e21de18e636d
Abi Noval Fauzi <noval@asi-asiapacific.com>
2025-09-16 08:33:18 +0700

    🧹 chore: remove unnecessary validation code

--- src/routes/auth/create-account/lib.ts
+++ src/routes/auth/create-account/lib.ts
@@ -2,10 +2,7 @@ import * as v from 'valibot';
 
 export const UsernameSchema = v.pipe(
 	v.string(),
-	v.regex(
-		/^(?!.*\.\.)(?!.*\s)[a-zA-Z0-9._]{2,32}$/,
-		'Username must be 2–32 characters, using letters, numbers, underscores, or dots, and cannot have consecutive dots or spaces'
-	)
+	v.regex(/^(?!.*\.\.)(?!.*\s)[a-zA-Z0-9._]{2,32}$/)
 );
 
 export const CreateAccountSchema = v.pipe(
@@ -13,11 +10,11 @@ export const CreateAccountSchema = v.pipe(
 		username: UsernameSchema,
 		password: v.pipe(
 			v.string(),
-			v.minLength(8, 'Try making your password at least 8 characters'),
-			v.maxLength(128, 'Keep your password under 128 characters'),
-			v.regex(/[a-z]/, 'Add at least one lowercase letter'),
-			v.regex(/[A-Z]/, 'Add at least one uppercase letter'),
-			v.regex(/[0-9]/, 'Add at least one number')
+			v.minLength(8),
+			v.maxLength(128),
+			v.regex(/[a-z]/),
+			v.regex(/[A-Z]/),
+			v.regex(/[0-9]/)
 		)
 	})
 );
