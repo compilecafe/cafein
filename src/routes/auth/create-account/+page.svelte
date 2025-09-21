4d672de5e3e9285f74a5c02b1bf2423ccee19909
pow <abinovalfauzi@proton.me>
2025-09-21 15:19:35 +0700

    📝 feat: update create account page to use new field component

--- src/routes/auth/create-account/+page.svelte
+++ src/routes/auth/create-account/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-	import noiseBg from '$assets/noise.png';
-	import LogoDarkWordmark from '$components/brand/logo-dark-wordmark.svelte';
+	import { Field } from '$components/forms/field';
 	import Alert from '$components/ui/alert.svelte';
 	import ButtonLink from '$components/ui/button-link.svelte';
 	import Button from '$components/ui/button.svelte';
 	import Icon from '$components/ui/icon.svelte';
-	import { showPassword, squircle } from '$lib/client/attachments';
 	import { cn } from '$lib/client/cn';
 	import { CreateAccountSchema, UsernameSchema } from './lib';
 	import { useDebounce } from 'runed';
@@ -88,132 +86,89 @@
 	};
 </script>
 
-<div
-	class="relative flex min-h-dvh items-center bg-primary-600 py-10"
-	style="background-image: url({noiseBg});"
->
-	<div class="inner">
-		<div
-			class="mx-auto flex w-full max-w-md flex-col items-center rounded-3xl bg-gray-100 p-10"
-			{@attach squircle(24)}
-		>
-			<a href="/" aria-label="Home"><LogoDarkWordmark height={40} /></a>
-			<h1 class="mt-4 mb-2 text-3xl">Welcome</h1>
-			<p>Let’s get you set up — it’s quick.</p>
-			<hr class="my-6 w-full border-t border-dashed border-gray-300" />
-			{#if $message}
-				<Alert message={$message} class="mb-4" />
-			{/if}
-			<form class="flex w-full flex-col gap-y-4" method="POST" use:enhance>
-				<div class="flex flex-col gap-2">
-					<label for="username" class="font-medium">Pick a username</label>
-					<div class="relative">
-						<input
-							type="text"
-							id="username"
-							name="username"
-							class="peer h-14 w-full rounded-xl border border-gray-300 pr-4 pl-12 transition-all focus:border-primary-600 focus:outline-none"
-							placeholder="e.g. cooldev123"
-							{@attach squircle()}
-							bind:value={$form.username}
-							oninput={handleChangeUsername}
-						/>
-						<Icon
-							icon="solar:user-bold-duotone"
-							class="absolute top-1/2 left-4 size-6 -translate-y-1/2 text-gray-400 transition-all peer-focus:text-primary-600"
-						/>
-					</div>
-					{#if usernameState === 'CHECKING'}
-						<div class="flex items-center gap-1">
-							<Icon
-								icon="solar:black-hole-3-bold-duotone"
-								class="animate-spin text-2xl text-info-600"
-							/>
-							<span class="text-sm font-medium text-info-600">
-								Checking username availability...
-							</span>
-						</div>
-					{:else if usernameState === 'UNAVAILABLE'}
-						<div class="flex items-center gap-1">
-							<Icon icon="solar:heart-broken-bold-duotone" class="text-2xl text-error-600" />
-							<span class="text-sm font-medium text-error-600">Username is not available</span>
-						</div>
-					{:else if usernameState === 'AVAILABLE'}
-						<div class="flex items-center gap-1">
-							<Icon icon="solar:confetti-bold-duotone" class="text-2xl text-success-600" />
-							<span class="text-sm font-medium text-success-600">Username is available</span>
-						</div>
-					{/if}
-					<p class={cn('text-sm text-gray-600', usernameState === 'INVALID' && 'text-error-600')}>
-						Username must be 2–32 chars, can include letters, numbers, underscore or full stops, no
-						spaces or repeating dots.
-					</p>
-				</div>
-				<div class="flex flex-col gap-2">
-					<label for="password" class="font-medium">Create a password</label>
-					<div class="relative">
-						<input
-							type="password"
-							id="password"
-							name="password"
-							class="peer h-14 w-full rounded-xl border border-gray-300 px-12 transition-all focus:border-primary-600 focus:outline-none"
-							placeholder="••••••••"
-							{@attach squircle()}
-							bind:value={$form.password}
-							oninput={handleChangePassword}
-						/>
-						<button
-							type="button"
-							class="group absolute top-1/2 right-4 -translate-y-1/2"
-							{@attach showPassword}
-						>
-							<Icon
-								icon="solar:eye-bold-duotone"
-								class="size-6 text-gray-400 data-[state=shown]:hidden"
-							/>
-							<Icon
-								icon="solar:eye-closed-bold-duotone"
-								class="hidden size-6 text-gray-400 data-[state=shown]:block"
-							/>
-						</button>
-						<Icon
-							icon="solar:lock-password-bold-duotone"
-							class="absolute top-1/2 left-4 size-6 -translate-y-1/2 text-gray-400 transition-all peer-focus:text-primary-600"
-						/>
-					</div>
-					<div class="space-y-1">
-						{#each passwordRequirements as requirement}
-							<div class="flex items-center gap-1">
-								{#if requirement.valid}
-									<Icon class="text-2xl text-success-600" icon="solar:confetti-bold-duotone" />
-								{:else}
-									<Icon class="text-2xl text-error-500" icon="solar:close-square-bold-duotone" />
-								{/if}
-								<span class="text-sm text-gray-600">
-									{requirement.text}
-								</span>
-							</div>
-						{/each}
-					</div>
-				</div>
-				<Button disabled={!buttonActive || $submitting}>
-					Create my account
-					{#if $submitting}
-						<Icon class="animate-spin" icon="solar:black-hole-3-bold-duotone" />
+<h1 class="mt-4 mb-2 text-3xl">Welcome</h1>
+<p>Let’s get you set up — it’s quick.</p>
+<hr class="my-6 w-full border-t border-dashed border-gray-300" />
+{#if $message}
+	<Alert message={$message} class="mb-4" />
+{/if}
+<form class="flex w-full flex-col gap-y-4" method="POST" use:enhance>
+	<Field.Root>
+		<Field.Label for="username">Pick a username</Field.Label>
+		<Field.Input
+			icon="solar:user-bold-duotone"
+			id="username"
+			name="username"
+			placeholder="e.g. cooldev123"
+			bind:value={$form.username}
+			oninput={handleChangeUsername}
+		/>
+	</Field.Root>
+	<div class="flex flex-col gap-2">
+		{#if usernameState === 'CHECKING'}
+			<div class="flex items-center gap-1">
+				<Icon icon="solar:black-hole-3-bold-duotone" class="animate-spin text-2xl text-info-600" />
+				<span class="text-sm font-medium text-info-600"> Checking username availability... </span>
+			</div>
+		{:else if usernameState === 'UNAVAILABLE'}
+			<div class="flex items-center gap-1">
+				<Icon icon="solar:heart-broken-bold-duotone" class="text-2xl text-error-600" />
+				<span class="text-sm font-medium text-error-600">Username is not available</span>
+			</div>
+		{:else if usernameState === 'AVAILABLE'}
+			<div class="flex items-center gap-1">
+				<Icon icon="solar:confetti-bold-duotone" class="text-2xl text-success-600" />
+				<span class="text-sm font-medium text-success-600">Username is available</span>
+			</div>
+		{/if}
+		<p class={cn('text-sm text-gray-600', usernameState === 'INVALID' && 'text-error-600')}>
+			Username must be 2–32 chars, can include letters, numbers, underscore or full stops, no spaces
+			or repeating dots.
+		</p>
+	</div>
+	<div class="flex flex-col gap-2">
+		<Field.Root>
+			<Field.Label for="password">Create a password</Field.Label>
+			<Field.Input
+				icon="solar:lock-password-bold-duotone"
+				id="password"
+				name="password"
+				placeholder="••••••••"
+				type="password"
+				bind:value={$form.password}
+				oninput={handleChangePassword}
+			/>
+		</Field.Root>
+		<div class="space-y-1">
+			{#each passwordRequirements as requirement}
+				<div class="flex items-center gap-1">
+					{#if requirement.valid}
+						<Icon class="text-2xl text-success-600" icon="solar:confetti-bold-duotone" />
 					{:else}
-						<Icon icon="solar:map-arrow-right-bold-duotone" />
+						<Icon class="text-2xl text-error-500" icon="solar:close-square-bold-duotone" />
 					{/if}
-				</Button>
-				<div class="flex items-center gap-2 text-sm font-medium whitespace-nowrap text-gray-500">
-					<hr class="my-6 w-full border-t border-dashed border-gray-300" />
-					Already have an account?
-					<hr class="my-6 w-full border-t border-dashed border-gray-300" />
+					<span class="text-sm text-gray-600">
+						{requirement.text}
+					</span>
 				</div>
-				<ButtonLink intent="ghost" href="/auth/sign-in">
-					Sign in
-					<Icon icon="solar:star-rainbow-bold-duotone" />
-				</ButtonLink>
-			</form>
+			{/each}
 		</div>
 	</div>
-</div>
+	<Button disabled={!buttonActive || $submitting}>
+		Create my account
+		{#if $submitting}
+			<Icon class="animate-spin" icon="solar:black-hole-3-bold-duotone" />
+		{:else}
+			<Icon icon="solar:map-arrow-right-bold-duotone" />
+		{/if}
+	</Button>
+	<div class="flex items-center gap-2 text-sm font-medium whitespace-nowrap text-gray-500">
+		<hr class="my-6 w-full border-t border-dashed border-gray-300" />
+		Already have an account?
+		<hr class="my-6 w-full border-t border-dashed border-gray-300" />
+	</div>
+	<ButtonLink intent="ghost" href="/auth/sign-in">
+		Sign in
+		<Icon icon="solar:star-rainbow-bold-duotone" />
+	</ButtonLink>
+</form>
