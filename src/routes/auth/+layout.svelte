a6344b600c4bf0509413ac113b7390d9cd6a0603
pow <abinovalfauzi@proton.me>
2025-09-21 15:19:35 +0700

    🏞️ feat: refactor auth layout to use noise background

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,5 +1,22 @@
 <script lang="ts">
+	import noiseBg from '$assets/noise.png';
+	import LogoDarkWordmark from '$components/brand/logo-dark-wordmark.svelte';
+	import { squircle } from '$lib/client/attachments';
+
 	let { children } = $props();
 </script>
 
-{@render children?.()}
+<div
+	class="relative flex min-h-dvh items-center bg-primary-600 py-10"
+	style="background-image: url({noiseBg});"
+>
+	<div class="inner">
+		<div
+			class="mx-auto flex w-full max-w-md flex-col items-center rounded-3xl bg-gray-100 p-10"
+			{@attach squircle(24)}
+		>
+			<a href="/" aria-label="Home"><LogoDarkWordmark height={40} /></a>
+			{@render children?.()}
+		</div>
+	</div>
+</div>
